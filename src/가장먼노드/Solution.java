2f6f7e2268bd24ed65aeef0de35c019d907fd7b7
Ruel <lunaticmoonlight@gmail.com>
2021-04-12 22:49:23 +0900

    programmers 가장 먼 노드

--- src/가장먼노드/Solution.java
+++ src/가장먼노드/Solution.java
@@ -1,54 +1,49 @@
 package 가장먼노드;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
-    static int[] distance;
-    static List<Integer>[] lists;
-
     public static void main(String[] args) {
-        int n = 11;
-        int[][] edge = {{1, 2}, {1, 3}, {2, 4}, {2, 5}, {3, 5}, {3, 6}, {4, 8}, {4, 9}, {5, 9}, {5, 10}, {6, 10}, {6, 11}};
+        int n = 6;
+        int[][] edge = {{3, 6}, {4, 3}, {3, 2}, {1, 3}, {1, 2}, {2, 4}, {5, 2}};
 
-        distance = new int[n];      // 1로부터의 거리를 저장할 공간
-        Arrays.fill(distance, 20000);
-        distance[0] = 0;    // 1로부터 1까지의 거리는 0
+        int[] distance = new int[n];    // 거리의 최소값을 저장할 distance 공간
+        boolean[] check = new boolean[n];   // 해당 노드를 방문했는지 체크할 boolean 공간
+        check[0] = true;    // 1번 -> 1번까지의 거리를 체크할 필요없으므로 true값 저장
 
-        lists = new List[n];
+        List<Integer>[] lists = new List[n];    // 각 지점들로부터 연결된 곳을 저장할 List 배열
         for (int i = 0; i < lists.length; i++)
-            lists[i] = new LinkedList<>();
+            lists[i] = new ArrayList<>();
 
-        for (int[] arr : edge) {        //lists 공간에 각 노드부터 연결된 노드의 리스트를 저장해두자.
+        for (int[] arr : edge) {    // 각 지점으로부터 연결된 곳을 저장한다
             int a = arr[0];
             int b = arr[1];
 
             lists[a - 1].add(b - 1);
             lists[b - 1].add(a - 1);
         }
-        findMinDistance(0);
+        Queue<Integer> order = new LinkedList<>();  // BFS로 순회를 돌자!
+        order.add(0);   // 0(1번 지점)에서 시작
 
-        int max = 0;        // 최대값이 몇개인지 구하는 부분
+        while (!order.isEmpty()) {  // 큐가 빌 때까지
+            int cur = order.poll(); // 값을 꺼내서
+            for (int next : lists[cur]) {   // 연결된 지점들을 체크
+                if (!check[next]) {     // 아직 방문하지 않은 곳이라면
+                    distance[next] = distance[cur] + 1;     // cur지점부터 하나 떨어진 곳이므로, distance[cur]+1 값을 저장해주자.
+                    check[next] = true;     // 방문했다고 체크!
+                    order.add(next);    // 그리고 다음 번엔 해당 지점으로부터 연결된 곳을 돌자!
+                }
+            }
+        }
+        int max = 0;    // 완성된 distance 배열에서 최대값의 개수를 찾자.
         int count = 0;
-        for (int i = 0; i < distance.length; i++) {
-            if (distance[i] == 20000)
-                continue;
-            if (max < distance[i]) {
-                max = distance[i];
+        for (int i : distance) {
+            if (max < i) {
+                max = i;
                 count = 1;
-            } else if (max == distance[i])
+            } else if (max == i)
                 count++;
         }
         System.out.println(count);
-    }
-
-    static void findMinDistance(int num) {
-        for (int i : lists[num]) {  // num값으로부터 이어져 있는 다른 노드들을 확인하자.
-            if (distance[i] > distance[num] + 1) {  //num값으로부터 이어져있는 노드가 가진 distance가 num까지의 distance + 1보다 크다면, distance +1 값으로 갱신.
-                distance[i] = distance[num] + 1;
-                findMinDistance(i);     // 값이 갱신된 노드만 재귀적으로 다음 단계를 진행한다. -> 사실 재귀로 푼다면 오버플로우 위험이 있다. BFS를 활용해보자.
-            }
-        }
     }
 }
