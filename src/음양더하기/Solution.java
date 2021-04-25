0c0ccf945513cc56c2835b7f33a009c4840ed37a
Ruel <lunaticmoonlight@gmail.com>
2021-04-25 16:36:18 +0900

    programmers 음양 더하기

--- src/음양더하기/Solution.java
+++ src/음양더하기/Solution.java
@@ -4,5 +4,14 @@ public class Solution {
     public static void main(String[] args) {
         int[] absolutes = {4, 7, 12};
         boolean[] signs = {true, false, true};
+
+        int total = 0;
+        for (int i = 0; i < absolutes.length; i++) {
+            if (!signs[i])
+                total += absolutes[i] * -1;
+            else
+                total += absolutes[i];
+        }
+        System.out.println(total);
     }
 }
