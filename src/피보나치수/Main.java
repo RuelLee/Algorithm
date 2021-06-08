7fa37110bb4b4a4b25c9044dc8b12453ad263be9
Ruel <lunaticmoonlight@gmail.com>
2021-06-08 20:40:42 +0900

    baekjoon 2747번 피보나치 수

--- src/피보나치수/Main.java
+++ src/피보나치수/Main.java
@@ -3,23 +3,23 @@ package 피보나치수;
 import java.util.Scanner;
 
 public class Main {
-    static int[] dp;
+    static int[] memo;
 
     public static void main(String[] args) {
         // 재귀를 통해 구하게 된다면, 연산 수가 너무 많다.
-        // DP를 사용해야하는데, n이 주어질 때, 0부터 n까지 이전 두 숫자를 더해가며 채우는 방법과
-        // 재귀를 사용하되 피보나치 값을 배열에 모두 저장해두고, 한번 구했던 피보나치 값은 바로 참조함으로 연산을 줄일 수 있다.
-        // 첫번째 방법은 한번만 값을 구할 때 유리하고, 두번째는 한번 저장해둔 피보나치 값을을 지속적으로 참조할 때 유리하다.
+        // DP를 사용하여 0부터 n까지 값을 선형적으로 계산하는 방법이 있고,
+        // memoization 을 활용하여, 값을 구한 적 있다면 구했던 값을 참조하는 식으로 연산을 줄여 구할 수도 있다.
+        // 여기선 메모이제이션을 활용하여 풀어보자.
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        dp = new int[46];
-        dp[1] = dp[2] = 1;
+        memo = new int[46];
+        memo[1] = memo[2] = 1;
         System.out.println(fibonacci(n));
     }
 
     static int fibonacci(int r) {
-        if (dp[r] == 0)
-            dp[r] = fibonacci(r - 1) + fibonacci(r - 2);
-        return dp[r];
+        if (memo[r] == 0)
+            memo[r] = fibonacci(r - 1) + fibonacci(r - 2);
+        return memo[r];
     }
 }
