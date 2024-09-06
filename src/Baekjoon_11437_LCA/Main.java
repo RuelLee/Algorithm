6ba533acf776418b3f23468fb7f317de9e964d42
Ruel <lunaticmoonlight@gmail.com>
2024-09-06 15:30:50 +0900

    baekjoon 11437번 LCA

--- src/Baekjoon_11437_LCA/Main.java
+++ src/Baekjoon_11437_LCA/Main.java
@@ -14,103 +14,99 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static List<List<Integer>> child;
-    static int[] depth;
-    static int[][] sparseArray;
-
     public static void main(String[] args) throws IOException {
-        // n개의 정점으로 이루어진 트리가 주어진다. 루트는 1번.
-        // 두 노드의 쌍이 주어질 때 가장 가까운 공통 조상이 몇 번인지 출력하라.
+        // n(2 <= n <= 50,000)개의 정점으로 이루어진 트리가 주어진다.
+        // m( 1<= m <= 10,000)개 쌍의 노드들이 주어질 때, 공통 조상이 몇 번인지 출력하라
         //
-        // 최소 공통 조상 문제
-        // n이 최대 5만개, 쿼리가 최대 1만개 주어지므로, 희소 배열을 사용한다.
-        // 희소 배열을 최소 공통 조상에 이용하면
-        // 자신의 2^n번째 조상들을 희소배열에 저장한다.
-        // 그 후 두 노드가 주어지면, 희소배열을 통해 두 노드의 깊이를 빠르게 맞춰주고
-        // 그 후 같은 조상을 발견할 때까지 거슬러 올라간다.
+        // 트리, 희소 배열 문제
+        // 최대 50_000개의 정점이 주어지므로 최대 깊이는 50_000 - 1이 될 수 있다.
+        // 따라서 희소배열로 sparseArray[노드개수][log2(노드개수-1)+1]로 희소배열의 크기를 정할 수 있다.
+        // 그 후, 루트 노드인 1부터 자식 노드들을 찾아가며
+        // 깊이와 희소 배열 값들을 채워나간다.
+        // 그 후 쿼리를 처리해주면 된다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        child = new ArrayList<>(n + 1);
+        // 희소 배열
+        int[][] sparseArray = new int[n + 1][(int) (Math.log(n - 1) / Math.log(2)) + 1];
+        
+        // 노드 연결 정보
+        List<List<Integer>> connections = new ArrayList<>();
         for (int i = 0; i < n + 1; i++)
-            child.add(new ArrayList<>());
-
+            connections.add(new ArrayList<>());
+        StringTokenizer st;
         for (int i = 0; i < n - 1; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
 
-            child.get(a).add(b);
-            child.get(b).add(a);
+            connections.get(a).add(b);
+            connections.get(b).add(a);
         }
-
-        // n개의 노드의 깊이를 저장.
-        depth = new int[n + 1];
-        // 1번 노드는 1
-        depth[1] = 1;
-        // 각 노드의 조상들을 저장할 희소 배열 공간.
-        sparseArray = new int[n + 1][(int) Math.ceil(Math.log(n) / Math.log(2))];
-        // 큐를 통해 차근차근 방문하며 희소배열을 채워나간다.
+        
+        // 깊이
+        int[] depths = new int[n + 1];
+        // BFS 탐색
         Queue<Integer> queue = new LinkedList<>();
-        // 1번부터 시작.
         queue.offer(1);
-        // 방문 체크 배열
-        boolean[] visited = new boolean[n + 1];
         while (!queue.isEmpty()) {
             int current = queue.poll();
-            // current 방문 체크
-            visited[current] = true;
-            // 그리고 자신의 깊이와 채워진 희소배열을 토대로 자신의 희소배열을 채워나간다.
-            for (int i = 1; i < Math.ceil(Math.log(depth[current]) / Math.log(2)); i++)
-                sparseArray[current][i] = sparseArray[sparseArray[current][i - 1]][i - 1];
-
-            // currnet의 자식 노드들 탐색.
-            for (int next : child.get(current)) {
-                // 방문했다면(=조상노드라면) 건너뛴다.
-                if (visited[next])
+            
+            // 부모 노드를 제외한 자식 노드들에 한해
+            for (int child : connections.get(current)) {
+                if (child == sparseArray[current][0])
                     continue;
-
-                // next의 2^0번 조상(=부모노드)는 currnet
-                sparseArray[next][0] = current;
-                // 깊이는 depth[current] + 1
-                depth[next] = depth[current] + 1;
-                // 큐에 next를 담아 다음번에 탐색한다.
-                queue.offer(next);
+                
+                // 자식 노드의 조상 노드로 current 기록
+                sparseArray[child][0] = current;
+                // 큐 추가
+                queue.offer(child);
+                // 깊이 계산
+                depths[child] = depths[current] + 1;
+                // 자식 노드의 희소배열을 채운다.
+                for (int i = 1; i <= Math.log(depths[child]) / Math.log(2); i++)
+                    sparseArray[child][i] = sparseArray[sparseArray[child][i - 1]][i - 1];
             }
         }
-
-        // m개의 쿼리.
+        
+        // m개의 쿼리 처리
         int m = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            // a노드와 b노드의 공통 조상을 찾는다.
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            sb.append(findLCA(a, b)).append("\n");
+
+            // 두 노드의 깊이가 다르다면 같게 맞춰준다.
+            while (depths[a] != depths[b]) {
+                int diff = Math.abs(depths[a] - depths[b]);
+                // 희소배열을 통해 2^k 제곱 만큼씩 깊이 차리를 줄인다.
+                int jump = (int) (Math.log(diff) / Math.log(2));
+                if (depths[a] > depths[b])
+                    a = sparseArray[a][jump];
+                else
+                    b = sparseArray[b][jump];
+            }
+
+            // 두 노드의 깊이가 같아졌다.
+            // 이제 가장 가까운 공통 조상을 찾는다.
+            while (sparseArray[a][0] != sparseArray[b][0]) {
+                // 희소 배열을 끝부터 살펴보며, 달라지는 가장 이른 지점으로 두 노드를 이동시킨다.
+                for (int j = (int) (Math.log(depths[a]) / Math.log(2)); j >= 0; j--) {
+                    if (sparseArray[a][j] != sparseArray[b][j]) {
+                        a = sparseArray[a][j];
+                        b = sparseArray[b][j];
+                        break;
+                    }
+                }
+            }
+            // 두 노드의 부모 노드가 같아졌다.
+            // a와 b가 같아지는 경우 또한 존재하므로
+            // 우선적으로 두 노드가 같은지 살펴보고 같다면 해당 노드를 출력
+            // 다르다면 두 노드의 부모 노드를 출력한다.
+            sb.append(a == b ? a : sparseArray[a][0]).append("\n");
         }
+        // 전체 답 출력
         System.out.print(sb);
-    }
-
-    // 두 노드의 최소 공통 조상 찾기.
-    static int findLCA(int a, int b) {
-        // 두 노드의 깊이가 서로 다르다면
-        while (depth[a] != depth[b]) {
-            // 차이에 log2를 취해 희소배열을 통해 최대 몇 개의 조상을 건너뛸 수 있는지 계산한다.
-            int diff = (int) (Math.log(Math.abs(depth[a] - depth[b])) / Math.log(2));
-            // 그리고 해당만큼 깊이가 더 깊은 쪽을 건너뛰어준다.
-            if (depth[a] > depth[b])
-                a = sparseArray[a][diff];
-            else
-                b = sparseArray[b][diff];
-        }
-
-        // 깊이는 같아진 상태
-        // 이제 하나씩 거슬러 올라가며 같은 조상인지 확인한다.
-        while (a != b) {
-            a = sparseArray[a][0];
-            b = sparseArray[b][0];
-        }
-        // a == b인 시점에 위 while문이 끝나고, a(= b) 값이 최소 공통 조상.
-        return a;
     }
 }
