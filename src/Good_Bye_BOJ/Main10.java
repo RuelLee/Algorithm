41ad19988b1c1c86f1c4c7fbfe91469810d91360
Ruel <lunaticmoonlight@gmail.com>
2026-04-26 18:44:35 +0900

    Good Bye, BOJ!

---
+++ src/Good_Bye_BOJ/Main10.java
@@ -0,0 +1,14 @@
+/*
+ Author : Ruel
+ Git hub : https://github.com/RuelLee
+ Mail Address : lunaticmoonlight@gmail.com
+*/
+
+package Good_Bye_BOJ;
+
+public class Main10 {
+    public static void main(String[] args) {
+        System.out.println("그동안 감사했습니다.");
+        System.out.println("Good Bye, BOJ!");
+    }
+}
